7b45feb331b8a0fa84f289a879740936b7066a65
Moses Mendoza <mendoza.moses@gmail.com>
2021-04-03 10:15:38 -0700

    continued work POCing

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import Depends, FastAPI
+
+from .routers import documents
+
+app = FastAPI(dependencies=[])
+
+app.include_router(documents.router)
+
+
+
---
+++ app/routers/documents.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/documents/", tags=["users"])
+async def read_documents():
+  return [{"document": "foo"}]
