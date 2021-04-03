13aadf186f0c3ddbe0eb516afc45817d872b0664
Moses Mendoza <mendoza.moses@gmail.com>
2021-04-03 11:08:58 -0700

    remove trailing slash on path

--- app/routers/documents.py
+++ app/routers/documents.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/documents/", tags=["users"])
+@router.get("/documents", tags=["users"])
 async def read_documents():
   return [{"document": "foo"}]
