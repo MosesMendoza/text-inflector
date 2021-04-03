fd0aa79a9ee1287ab36f78b6b9ebd7a306bc4117
Moses Mendoza <mendoza.moses@gmail.com>
2021-04-03 11:19:33 -0700

    Docker build work
    copy POC code over to server poc
    update dockerfile to install requirements
    Add download of corpora data to Dockerfile
    
    Signed-off-by: Moses Mendoza <mendoza.moses@gmail.com>
    
    make a basic document model

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,11 @@
-FROM tiangolo/uvicorn-gunicorn-fastapi:python3.7
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.7-2020-12-19
+
+COPY requirements.txt /
+
+RUN pip install -r /requirements.txt
+
+ENV NLTK_DATA=/
+# Sorry container size
+RUN python -m textblob.download_corpora
 
 COPY ./app /app/app
--- app/routers/documents.py
+++ app/routers/documents.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
+from textblob import TextBlob
+from models import document
 
 router = APIRouter()
 
-@router.get("/documents", tags=["users"])
-async def read_documents():
-  return [{"document": "foo"}]
+@router.post("/tags", tags=["users"])
+async def getTags(document: Document):
+  text = document.text
+  txtObj = TextBlob(text)
+  tags = txtObj.tags
+  return [{"document": tags}]
