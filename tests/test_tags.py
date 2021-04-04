6a3847671450cd8ea8677c254a549cf82237c6ce
Moses Mendoza <mendoza.moses@gmail.com>
2021-04-04 14:32:47 -0700

    specs for tags path
    Signed-off-by: Moses Mendoza <mendoza.moses@gmail.com>

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -3,12 +3,31 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_post_tags():
-  text = "I am a pear"
+def test_post_tags_returns_parts_of_speech():
+  text = "I am a pear."
   response = client.post(
     "/tags",
     json={
       "text": text
     }
   )
-  assert response.json() == {"tags": 'foo'}
+  assert response.json() == {"tags": [["I", "PRP"], ["am", "VBP"], ["a", "DT"], ["pear", "NN"]]}
+
+def test_post_tags_without_body_returns_422():
+  response = client.post(
+    "/tags"
+  )
+  assert response.status_code == 422
+
+
+def test_post_tags_without_text_field_returns_422():
+  response = client.post(
+    "/tags",
+    json={}
+  )
+  assert response.status_code == 422
+
+
+def test_get_tags_returns_405():
+  response = client.get("/tags")
+  assert response.status_code == 405
